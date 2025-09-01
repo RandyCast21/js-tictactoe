f14bd671aa885c99835525f04cb78f67dfdcba25
RandyCast21 <randycast21@gmail.com>
2025-09-01 16:33:49 -0600

    Conversion de la logica del GameBoard a un GameBoardModule, variables privadas y función win() publica

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-//GameBoard
-let fila1 = [1,2,2]
-let fila2 = [1,2,6]
-let fila3 = [1,5,2]
+//gameBoard
+const gameBoardModule = (function (){
+    let fila1 = [1,2,2]
+    let fila2 = [1,2,6]
+    let fila3 = [1,5,2]
 
-let GameBoard = [fila1, fila2, fila3];
+    let gameBoardField = [fila1, fila2, fila3];
 
-function win() {
-    //Victoria diagonal X2Y2, X1Y1, X3Y3
-    if(GameBoard[0][0] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[2][2]){
-        console.log("win diagonal derecha inferior")
-    }
-
-    //Victoria diagonal X2Y2, X1Y3, X3Y1
-    if(GameBoard[0][2] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[2][0]){
-        console.log("win diagonal derecha superior")
-    }
-
-    for (i = 0; i < 3; i++){
-        //Victoria por 3 horizontal
-        if(GameBoard[i][0] == GameBoard[i][1] && GameBoard[i][1] == GameBoard[i][2]) {
-            console.log("win horizontal")
+    //Función que verifica el ganador en el tablero
+    function win() {
+        //Victoria diagonal X2Y2, X1Y1, X3Y3
+        if(gameBoardField[0][0] == gameBoardField[1][1] && gameBoardField[1][1] == gameBoardField[2][2]){
+            console.log("win diagonal derecha inferior")
         }
 
-        //Victoria por 3 vertical
-        if(GameBoard[0][i] == GameBoard[1][i] && GameBoard[1][i] == GameBoard[2][i]) {
-            console.log("win vertical")
+        //Victoria diagonal X2Y2, X1Y3, X3Y1
+        if(gameBoardField[0][2] == gameBoardField[1][1] && gameBoardField[1][1] == gameBoardField[2][0]){
+            console.log("win diagonal derecha superior")
+        }
+
+        for (i = 0; i < 3; i++){
+            //Victoria por 3 horizontal
+            if(gameBoardField[i][0] == gameBoardField[i][1] && gameBoardField[i][1] == gameBoardField[i][2]) {
+                console.log("win horizontal")
+            }
+
+            //Victoria por 3 vertical
+            if(gameBoardField[0][i] == gameBoardField[1][i] && gameBoardField[1][i] == gameBoardField[2][i]) {
+                console.log("win vertical")
+            }
         }
     }
-}
 
-win();
+    return {win};
+})();
+
+gameBoardModule.win();
